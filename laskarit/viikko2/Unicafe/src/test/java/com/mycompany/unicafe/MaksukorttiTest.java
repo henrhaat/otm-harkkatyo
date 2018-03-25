61eea2207890f30c97b902618f5c050c99551fc2
Henri Haatanen <henri.haatanen@helsinki.fi>
2018-03-26 02:07:18 +0300

    Unicafe testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldonKasvattaminenToimiiOikein() {
+        kortti.lataaRahaa(120);
+        assertEquals("saldo: 1.30", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.otaRahaa(3);
+        assertEquals("saldo: 0.07", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosEiTarpeeksiRahaa() {
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinBooleanTrue() {
+        assertEquals(kortti.otaRahaa(5),true);
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinBooleanFalse() {
+        assertEquals(kortti.otaRahaa(11),false);
+    }
 }
