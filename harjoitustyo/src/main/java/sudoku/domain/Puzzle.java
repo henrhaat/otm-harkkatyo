862daa5a6dda0e000373bb067092529369dfcd48
Henri Haatanen <henri.haatanen@helsinki.fi>
2018-04-09 21:25:54 +0300

    laitettiin turha osa luokasta Puzzle kommentiksi

--- harjoitustyo/src/main/java/sudoku/domain/Puzzle.java
+++ harjoitustyo/src/main/java/sudoku/domain/Puzzle.java
@@ -2,7 +2,7 @@ package sudoku.domain;
 
 public class Puzzle {
 
-    private int[][] numbers = {{4, 3, 5, 2, 6, 9, 7, 8, 1},
+    /*private int[][] numbers = {{4, 3, 5, 2, 6, 9, 7, 8, 1},
     {6, 8, 2, 5, 7, 1, 4, 9, 3},
     {1, 9, 7, 8, 3, 4, 5, 6, 2},
     {8, 2, 6, 1, 9, 5, 3, 4, 7},
@@ -10,16 +10,16 @@ public class Puzzle {
     {9, 5, 1, 7, 4, 3, 6, 2, 8},
     {5, 1, 9, 3, 2, 6, 8, 7, 4},
     {2, 4, 8, 9, 5, 7, 1, 3, 6},
-    {7, 6, 3, 4, 1, 8, 2, 5, 9}};
+    {7, 6, 3, 4, 1, 8, 2, 5, 9}};*/
     
-    private int[] numbers1 = {4,3,5,6,8,2,1,9,7,2,6,9,5,7,1,8,3,4,7,8,1,4,9,3,5,6,2,
-                              8,2,6,3,7,4,9,5,1,1,9,5,6,8,2,7,4,3,3,4,7,9,1,5,6,2,8,
-                              5,1,9,2,4,8,7,6,3,3,2,6,9,5,7,4,1,8,8,7,4,1,3,6,2,5,9};
+    private final int[] numbers1 = {4,3,5,6,8,2,1,9,7,2,6,9,5,7,1,8,3,4,7,8,1,4,9,3,5,6,2,
+                                    8,2,6,3,7,4,9,5,1,1,9,5,6,8,2,7,4,3,3,4,7,9,1,5,6,2,8,
+                                    5,1,9,2,4,8,7,6,3,3,2,6,9,5,7,4,1,8,8,7,4,1,3,6,2,5,9};
 
     private final boolean t = true;
     private final boolean f = false;
 
-    private boolean[][] shown = {{f, f, f, t, t, f, t, f, t},
+    /*private boolean[][] shown = {{f, f, f, t, t, f, t, f, t},
     {t, t, f, f, t, f, f, t, f},
     {t, t, f, f, f, t, t, f, f},
     {t, t, f, t, f, f, f, t, f},
@@ -27,23 +27,23 @@ public class Puzzle {
     {f, t, f, f, f, t, f, t, t},
     {f, f, t, t, f, f, f, t, t},
     {f, t, f, f, t, f, f, t, t},
-    {t, f, t, f, t, t, f, f, f}};
+    {t, f, t, f, t, t, f, f, f}};*/
     
-    private boolean[] shown1 = {f,f,f,t,t,f,t,t,f,t,t,f,f,t,f,f,f,t,t,f,t,f,t,f,t,f,f,
-                                t,t,f,f,f,t,f,t,f,t,f,f,t,f,t,f,f,t,f,t,f,t,f,f,f,t,t,
-                                f,f,t,f,t,f,t,f,t,t,f,f,f,t,f,f,t,t,f,t,t,f,t,t,f,f,f};
+    private final boolean[] shown1 = {f,f,f,t,t,f,t,t,f,t,t,f,f,t,f,f,f,t,t,f,t,f,t,f,t,f,f,
+                                      t,t,f,f,f,t,f,t,f,t,f,f,t,f,t,f,f,t,f,t,f,t,f,f,f,t,t,
+                                      f,f,t,f,t,f,t,f,t,t,f,f,f,t,f,f,t,t,f,t,t,f,t,t,f,f,f};
     
     public Puzzle() {
 
     }
     
-    public int[][] getNumbers() {
+    /*public int[][] getNumbers() {
         return this.numbers;
     }
     
     public boolean[][] getShown() {
         return this.shown;
-    }
+    }*/
     
     public int[] getNumbers1() {
         return this.numbers1;
