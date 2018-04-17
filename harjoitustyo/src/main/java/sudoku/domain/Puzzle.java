e3439aa7441ee467527ec420eceb28df24231fcb
Henri Haatanen <henri.haatanen@helsinki.fi>
2018-04-17 21:31:43 +0300

    checkstyle korjaus

--- harjoitustyo/src/main/java/sudoku/domain/Puzzle.java
+++ harjoitustyo/src/main/java/sudoku/domain/Puzzle.java
@@ -5,27 +5,26 @@ import java.util.List;
 
 public class Puzzle {
 
-    
     private List<boolean[]> shownList;
     private List<String[]> numberList;
-    
-    
+
     public final String[] numbersString = {"4", "3", "5", "6", "8", "2", "1", "9", "7", "2", "6", "9", "5", "7", "1", "8", "3", "4", "7", "8", "1", "4", "9", "3", "5", "6", "2",
-                                            "8", "2", "6", "3", "7", "4", "9", "5", "1", "1", "9", "5", "6", "8", "2", "7", "4", "3", "3", "4", "7", "9", "1", "5", "6", "2", "8",
-                                            "5", "1", "9", "2", "4", "8", "7", "6", "3", "3", "2", "6", "9", "5", "7", "4", "1", "8", "8", "7", "4", "1", "3", "6", "2", "5", "9"};
+        "8", "2", "6", "3", "7", "4", "9", "5", "1", "1", "9", "5", "6", "8", "2", "7", "4", "3", "3", "4", "7", "9", "1", "5", "6", "2", "8",
+        "5", "1", "9", "2", "4", "8", "7", "6", "3", "3", "2", "6", "9", "5", "7", "4", "1", "8", "8", "7", "4", "1", "3", "6", "2", "5", "9"};
 
     private final boolean t = true;
     private final boolean f = false;
-    
+
     public final boolean[] shown = {f, f, f, t, t, f, t, t, f, t, t, f, f, t, f, f, f, t, t, f, t, f, t, f, t, f, f,
-                                     t, t, f, f, f, t, f, t, f, t, f, f, t, f, t, f, f, t, f, t, f, t, f, f, f, t, t,
-                                     f, f, t, f, t, f, t, f, t, t, f, f, f, t, f, f, t, t, f, t, t, f, t, t, f, f, f};    
+        t, t, f, f, f, t, f, t, f, t, f, f, t, f, t, f, f, t, f, t, f, t, f, f, f, t, t,
+        f, f, t, f, t, f, t, f, t, t, f, f, f, t, f, f, t, t, f, t, t, f, t, t, f, f, f};
+
     public Puzzle() {
         this.numberList = new ArrayList<>();
         this.shownList = new ArrayList<>();
         setUp();
     }
-    
+
     private void setUp() {
         this.numberList.add(numbersString);
         this.shownList.add(shown);
@@ -34,7 +33,7 @@ public class Puzzle {
     public boolean[] getShown(int n) {
         return this.shownList.get(n);
     }
-    
+
     public String[] getNumbersString(int n) {
         return this.numberList.get(n);
     }
